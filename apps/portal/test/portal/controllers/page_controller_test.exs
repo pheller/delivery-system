134b2fba2fefe5952bec50a5497d58c3ed66ed39
Phillip Heller <pheller@me.com>
2023-06-02 16:11:41 -0600

    Add scaffolding for portal app
    - boilerplate from mix phx.new.web
    - added PubSub config to lib/portal/application.ex
    - fixed assets/tailwind.confg.js content paths under lib; generator appends _web where it shouldn't.

---
+++ apps/portal/test/portal/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule Prodigy.Portal.PageControllerTest do
+  use Prodigy.Portal.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
